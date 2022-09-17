56c8f684b7ea51a81bfdd1d52ee81051b42f3b03
XLR8 <xlr8.19@gmail.com>
2022-09-17 23:00:14 +0300

    BaseStructure class

--- src/Models/Memory/BaseStructureMemory.ts
+++ src/Models/Memory/BaseStructureMemory.ts
@@ -1,7 +1,9 @@
+import { StructureTypes } from "Models/Structures/BaseStructure";
 import { CreepTypes } from "../Creeps/BaseCreep";
 
 export class BaseStructureMemory
 {
+    Type:StructureTypes=0;
     targetID: string;
     actionAttempts: number = 0;
     taskNumber: number = 0;
