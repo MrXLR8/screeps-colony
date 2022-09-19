96a5aa074b2ee76b6db70fe2e6ddf4f5093f61d0
XLR8 <xlr8.19@gmail.com>
2022-09-19 15:53:10 +0300

    Gather v2

---
+++ src/Logic/Actions/Carry/ActionGather.ts
@@ -0,0 +1,94 @@
+import { Finder } from "Logic/Finder";
+import { ActionResponseCode } from "Models/ActionResponseCode";
+import { BaseCreep } from "Models/Creeps/BaseCreep";
+import { IAction } from "../IAction";
+
+export class ActionGather implements IAction
+{
+    unit: BaseCreep;
+    target: StructureContainer | StructureStorage;
+
+    constructor(creep: BaseCreep)
+    {
+        this.unit = creep as BaseCreep;
+    }
+
+
+    Act(): ActionResponseCode
+    {
+        var entryCode = this.EntryValidation();
+        if (!entryCode) return entryCode;
+
+        this.GetSavedTarget();
+
+        if (this.target == null) return ActionResponseCode.NextTask;
+
+        var actionCode = this.unit.creep.withdraw(this.target, RESOURCE_ENERGY);
+
+        return this.WorkCodeProcessing(actionCode);
+    }
+
+    EntryValidation(): ActionResponseCode
+    {
+        if (this.unit.creep.store.getFreeCapacity() == 0) return ActionResponseCode.NextTask;
+        return null;
+    }
+
+    GetSavedTarget(): void
+    {
+        var targetId = this.unit.targetId;
+        if (targetId == null)
+        {
+            this.target = Game.getObjectById(this.unit.targetId as Id<StructureContainer | StructureStorage>);
+        }
+        if (this.target != null)
+        {
+            if (this.target.store.getUsedCapacity(RESOURCE_ENERGY) > this.unit.AmmountCanCarry())
+            {
+                return; //Target is valid
+            }
+        }
+
+        this.target = Finder.GetFilledStorage(this.unit.creep.pos, this.unit.AmmountCanCarry());
+        if (this.target != null)
+        {
+            this.unit.targetId = this.target.id;
+        }
+    }
+
+    RepeatAction(): boolean
+    {
+        var newTarget = Finder.GetFilledStorage
+            (
+                this.unit.creep.pos,
+                this.unit.AmmountCanCarry(),
+                this.unit.targetId as Id<StructureContainer | StructureStorage>
+            );
+        if (newTarget != null)
+        {
+            this.unit.targetId = newTarget.id;
+            this.unit.MoveToTarget(newTarget);
+            return true;
+        }
+        return false;
+    }
+
+    WorkCodeProcessing(code: ScreepsReturnCode): ActionResponseCode
+    {
+        switch (code)
+        {
+            case ERR_NOT_IN_RANGE:
+                this.unit.MoveToTarget(this.target);
+                this.unit.creep.say(">⚡");
+                return ActionResponseCode.Repeat;
+            case OK:
+                this.unit.creep.say("⚡");
+                this.unit.memory.actions.worked = true;
+                if (!this.RepeatAction()) return ActionResponseCode.NextTask;
+                return ActionResponseCode.Repeat;
+            default:
+                this.unit.log("Problem occured. Gather error code: " + code);
+                return ActionResponseCode.NextTask;
+        }
+    }
+}
---
+++ src/Logic/Actions/IAction.ts
@@ -0,0 +1,19 @@
+import { ActionResponseCode } from "Models/ActionResponseCode";
+import { Unit } from "Models/Unit";
+
+export interface IAction
+{
+
+
+ Act():ActionResponseCode;
+
+ EntryValidation():ActionResponseCode;
+
+ GetSavedTarget():void;
+
+
+ RepeatAction():boolean;
+
+ WorkCodeProcessing(code:number):ActionResponseCode;
+
+}
