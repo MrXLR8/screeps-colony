496139ed61ca971cfcf177bd9c716e30fc15376b
XLR8 <xlr8.19@gmail.com>
2022-09-20 10:31:32 +0300

    ActionTowerAttack

---
+++ src/Logic/Actions/Strcuture/ActionTowerAttack.ts
@@ -0,0 +1,74 @@
+import { Finder } from "Logic/Finder";
+import { ActionResponseCode } from "Models/ActionResponseCode";
+import { BaseCreep } from "Models/Creeps/BaseCreep";
+import { BaseStructure } from "Models/Structures/BaseStructure";
+import { IAction } from "../IAction";
+import {Tower} from "Models/Structures/Tower";
+export class ActionTowerAttack implements IAction
+{
+    unit: Tower;
+    target: Creep;
+
+    constructor(structure: BaseStructure)
+    {
+        this.unit = structure as Tower;
+    }
+
+    Act(): ActionResponseCode
+    {
+        var entryCode = this.EntryValidation();
+        if (!entryCode) return entryCode;
+
+        this.GetSavedTarget();
+
+        if (this.target == null) return ActionResponseCode.NextTask;
+
+        var actionCode = this.unit.structure.attack(this.target);
+
+        return this.WorkCodeProcessing(actionCode);
+    }
+
+    EntryValidation(): ActionResponseCode
+    {
+        if (this.unit.structure.store[RESOURCE_ENERGY] == 0) return ActionResponseCode.NextTask;
+        return null;
+    }
+
+    GetSavedTarget(): void
+    {
+        var targetId = this.unit.targetId;
+        if (targetId == null)
+        {
+            this.target = Game.getObjectById(this.unit.targetId as Id<Creep>);
+        }
+        if (this.target != null)
+                return; //Target is valid
+
+        this.target = this.unit.structure.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (this.target != null)
+        {
+            this.unit.targetId = this.target.id;
+        }
+    }
+
+    WorkCodeProcessing(code: ScreepsReturnCode): ActionResponseCode
+    {
+        switch (code)
+        {
+            case OK:
+                this.unit.memory.actions.worked=true;
+                 return ActionResponseCode.Repeat;
+            case ERR_NOT_ENOUGH_RESOURCES|ERR_NOT_ENOUGH_ENERGY:
+                Game.notify("TOWER CANNOT ATTACK. LOW ON RESOURCES");
+                return ActionResponseCode.Repeat;
+            default:
+                this.unit.log("Problem occured. TowerAttack error code: " + code);
+                return ActionResponseCode.NextTask;
+        }
+    }
+
+    RepeatAction(): boolean
+    {
+        throw("Not Implemented");
+    }
+}
