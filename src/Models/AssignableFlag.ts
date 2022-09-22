31150b180100fa0536b17f7ea937db8e0b836171
XLR8 <xlr8.19@gmail.com>
2022-09-22 14:59:33 +0300

    assignableflag trytoassign

--- src/Models/AssignableFlag.ts
+++ src/Models/AssignableFlag.ts
@@ -20,41 +20,48 @@ export class AssignableFlag
         this.flag.memory = memory;
     }
 
-    get assignedAmmount() :number
+    get assignedAmmount(): number
     {
         return this.memory.assignedCreeps.length;
     }
 
-  public  isAssigned(creepId: string): boolean
+    public isAssigned(creepId: string): boolean
     {
         return this.memory.assignedCreeps.includes(creepId)
     }
 
-    public  Assign(creepId: string)
+    public Assign(creepId: string)
     {
 
-        console.log("assigning to the flag "+this.flag.name+" next creep: "+creepId+" count: "+this.memory.assignedCreeps.length);
+        console.log("assigning to the flag " + this.flag.name + " next creep: " + creepId + " count: " + this.memory.assignedCreeps.length);
         this.memory.assignedCreeps.push(creepId);
     }
 
-    public  Release(creepId: string)
+    public TryToAssign(creepId: string, maxAmmount: number): boolean
+    {
+        if (this.isAssigned(creepId)) return true;
+        if (this.assignedAmmount >= maxAmmount) return false;
+        return true;
+    }
+
+    public Release(creepId: string)
     {
         this.memory.assignedCreeps = this.memory.assignedCreeps.filter(data => data != creepId);
     }
 
     public ReleaseDead()
     {
-        for(var creepId of this.memory.assignedCreeps)
+        for (var creepId of this.memory.assignedCreeps)
         {
             var found = Game.getObjectById<Id<Creep>>(creepId as Id<Creep>);
-            if(found==null) this.Release(creepId);
+            if (found == null) this.Release(creepId);
         }
     }
 
 
-    public CompareColors(primaryColor:ColorConstant,secondaryColor:ColorConstant)
+    public CompareColors(primaryColor: ColorConstant, secondaryColor: ColorConstant)
     {
-        return this.flag.color==primaryColor&&this.flag.secondaryColor==secondaryColor;
+        return this.flag.color == primaryColor && this.flag.secondaryColor == secondaryColor;
     }
 
 }
