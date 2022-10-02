d4bfab9956e2a72c4b50980bf1a0eda1e148ac42
XLR8 <xlr8.19@gmail.com>
2022-10-02 21:13:48 +0300

    traveller movment

--- src/Logic/Actions/Basic/ActionMoveOrigin.ts
+++ src/Logic/Actions/Basic/ActionMoveOrigin.ts
@@ -40,7 +40,7 @@ export class ActionMoveOrigin implements IAction
         return null;
     }
 
-    private WorkCodeProcessing(code: CreepMoveReturnCode | ERR_NO_PATH | ERR_INVALID_TARGET | ERR_NOT_FOUND): ActionResponseCode
+    private WorkCodeProcessing(code:number): ActionResponseCode
     {
         switch (code)
         {
